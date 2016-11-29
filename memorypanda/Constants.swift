3ee37fc06175a7db2cabe56abf84c0f0c1d01fc6
Robbie Santos <robbiealixsantos@gmail.com>
2016-11-28 20:16:13 -0800

    First commit

---
+++ memorypanda/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  memorypanda
+//
+//  Created by Robbie Santos on 11/28/16.
+//  Copyright (c) 2016 Robbie Santos. All rights reserved.
+//
+
+import Foundation
+
+let BackgroundImage = "Background"
+
+let buttonPlayImage = "b_play"
+let buttonLeaderboardImage = "b_rang"
+let buttonRateImage = "b_rate"
+let buttonResultImage = "b_rate"
+let titleImage = "title"
+let scoreboardImage = "scoreboard"
+let finishedFlagImage = "finished"
+let buttonRestartImage = "b_restart"
+
